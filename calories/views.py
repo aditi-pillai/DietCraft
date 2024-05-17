7c818087bf27bdfd9982dd85f0c9e273eab1dbc9
Aditi Pillai <write2aditipillai@gmail.com>
2024-05-17 22:44:39 +0530

    upload

--- calories/views.py
+++ calories/views.py
@@ -7,7 +7,7 @@ def calorie_counter(request):
     import json
     import requests
     if request.method == 'POST':
-        query = request.POST['query']
+        query = request.POST['query'] 
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
         api_request = requests.get(
             api_url + query, headers={'X-Api-Key': 'yy+fGlsQMnVpqS9gdGdatg==GTWbYZnLQoF3t1IH'})
@@ -17,9 +17,9 @@ def calorie_counter(request):
         except Exception as e:
             api = "oops! There was an error"
             print(e)
-        return render(request, 'calories/calorie_counter.html', {'api': api})
+        return render(request, 'calories/calorie_counter_new2.html', {'api': api})
     else:
-        return render(request, 'calories/calorie_counter.html', {'query': 'Enter a valid query'})
+        return render(request, 'calories/calorie_counter_new2.html', {'query': 'Enter a valid query'})
         
 # def calorie_counter(request):
 #     return render(request, 'calories/calorie_counter_new.html')
